a6011a9c43704fb5404c4cdac823928997a9d244
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2024-10-18 12:37:25 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace birthday
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public int Age
+        {
+            get
+            {
+                return (DateTime.Now - Birthday).Days / 365;
+            }
+        }
+
+        public DateTime Birthday { get; private set; }
+        public User(string name, DateTime birthday)
+        {
+            Name = name;
+            Birthday = birthday;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User("Deepanshi", new DateTime(1990,10,3));
+            Console.WriteLine(user.Age);
+        }
+    }
+}
